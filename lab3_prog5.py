ced47816020e6ba879ccda09acec82644a5c75e1
Claire Rhoda <clairehrhoda@gmail.com>
2019-03-29 19:10:04 -0700

    added for loop

--- lab3_prog5.py
+++ lab3_prog5.py
@@ -1,19 +1,16 @@
 #Claire Rhoda
 #1068768
 #Program 5
-#This program gets the first, middle, and last name of a person and
+#This program takes the inputed first, middle, and last name of a person and
 #displays their initials
 
-def fullname()->(str):
-    user_input = input("Enter yout full name")
-    return (user_input)
+def separate(user_input:str)->(str,str,str):
+    sections = user_input.split( )
+    for name in sections:
+        first,middle,last = name[0]
+        return(first,middle,last)
+   
 
-def separate(firstMiddleLast:str)->(str,str,str):
-    sections = firstMiddleLast.split(' ')
-    first = sections[0]
-    middle = sections[1]
-    last = sections[2]
-    return (first,middle,last)
 
 def getInitials(first:str,middle:str,last:str)->str:
     l1 = first[0:1]
@@ -22,11 +19,13 @@ def getInitials(first:str,middle:str,last:str)->str:
     Initials = (l1 +"." + l2 + "." + l3 + ".")
     return Initials
 
+
 def main():
-    firstMiddleLast = fullname()
-    first,middle,last = separate(firstMiddleLast)
+    user_input = input("Enter yout full name")
+    first,middle,last = separate(user_input)
     initials = getInitials(first,middle,last)
     print(initials)
+
 
 if __name__ == "__main__":
     main()
