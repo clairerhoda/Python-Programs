5dfbcf06abc74338cbca6b32d2e690608d102c13
Claire Rhoda <clairehrhoda@gmail.com>
2019-04-10 16:28:49 -0700

    lab4 prog 2

---
+++ lab4_prog2.py
@@ -0,0 +1,19 @@
+def main():
+    numbers = makeList()
+    odd = delEven(numbers)
+    print("Original List-",numbers)
+    print("List after deleting even numbers:",odd)
+
+def delEven(numbers:list)->list:
+    odd = []
+    for even in numbers:
+        if numbers[even] % 2 != 0: 
+            odd = odd.remove(even)
+            return odd
+
+def makeList()->list:
+    numbers = list(range(1,11))
+    return numbers
+
+if __name__ == "__main__":
+    main()
