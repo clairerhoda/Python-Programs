b7e9fff954a7bff907364709c73b74f662135f66
Claire Rhoda <clairehrhoda@gmail.com>
2019-04-17 18:52:38 -0700

    lab4_prog5.py

---
+++ lab4_prog5.py
@@ -0,0 +1,26 @@
+#Claire Rhoda
+#1068768
+#Program 5
+#This program highlights the largest number from inputed numbers
+
+def readValues():
+    key = 'Q'
+    num_list = []
+    while key != 'Q':
+        print("Please enter values, Q to quit")
+        num = float(input())
+        num = append(num)
+
+
+
+def findLargest():
+    sort()
+    reverse()
+    largest = [0:1]
+
+def display():
+
+
+def main():
+    readValues()
+
