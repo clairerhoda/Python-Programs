c4a0796c66fb42f03987e00210f7a6803ccb8780
Claire Rhoda <clairehrhoda@gmail.com>
2019-04-19 14:11:00 -0700

    changes

--- lab4_prog6.py
+++ lab4_prog6.py
@@ -5,65 +5,82 @@
 
 
 def are_anagrams(word1,word2)->None:
-    listWord1 = []
+    """calculates if the two words entered are anagrams"""
+    listWord1 = []  #creates lists for letters to be separated
     listWord2 = []
     for letters in word1:
         listWord1.append(letters)
-        listWord1.sort()
+        listWord1.sort()   #sorts the letters in order
     for letters1 in word2:
         listWord2.append(letters1)
-        listWord2.sort()
-    if listWord1 == listWord2:
+        listWord2.sort()  #sort the letters in order
+    if listWord1 == listWord2:   #checks if the two lists of letters are equal 
         print("The words",word1,"and",word2,"are anangrams.")
     else:
         print("The words",word1,"and",word2,"are not anangrams.")
 
 
 def main():
+    """tests if input equals two words"""
     try:
         two_words = input("Enter two space separated words:")
         word1,word2 = two_words.split() #splits the string
-        are_anagrams(word1,word2)
+        are_anagrams(word1,word2)  #calls anagram function and gives words
 
     except ValueError:
-        print("Bad Input")
+        print("Bad Input")  #if wrong number of words are inputed, error is shown
 
 if __name__ == "__main__":
     main()
 
-# Test Case 1
-# >>>
-# =============== RESTART: /Users/clairerhoda/Documents/test.py ===============
-# Enter two space separated words:listen silent
-# The words listen and silent are anangrams.
+input("\nPress Enter to quit")
 
-# Test Case 2
-# >>>
-# =============== RESTART: /Users/clairerhoda/Documents/test.py ===============
-# Enter two space separated words:good dog god
-# Bad Input
 
-# Test Case 3
-# >>>
-# =============== RESTART: /Users/clairerhoda/Documents/test.py ===============
-# Enter two space separated words:wrong
-# Bad Input
-
-# Test Case 4
-# >>>
-# =============== RESTART: /Users/clairerhoda/Documents/test.py ===============
-# Enter two space separated words:fred
-# Bad Input
-
-# Test Case 5
-# >>>
-# =============== RESTART: /Users/clairerhoda/Documents/test.py ===============
-# Enter two space separated words:fred joe mary
-# Bad Input
-
-# Test Case 6
-# >>>
-# =============== RESTART: /Users/clairerhoda/Documents/test.py ===============
-# Enter two space separated words:cinema iceman
-# The words cinema and iceman are anangrams.
-# >>>
+##Test Case 1
+##>>> 
+##====== RESTART: /Users/clairerhoda/workspace/comp_sci_10/lab4_prog6.py ======
+##Enter two space separated words:fred
+##Bad Input
+##
+##Press Enter to quit
+##
+##Test Case 2
+##>>> 
+##====== RESTART: /Users/clairerhoda/workspace/comp_sci_10/lab4_prog6.py ======
+##Enter two space separated words:fred mary joe
+##Bad Input
+##
+##Press Enter to quit
+##
+##Test Case 3
+##>>> 
+##====== RESTART: /Users/clairerhoda/workspace/comp_sci_10/lab4_prog6.py ======
+##Enter two space separated words:cinema iceman
+##The words cinema and iceman are anangrams.
+##
+##Press Enter to quit
+##
+##Test Case 4
+##>>> 
+##====== RESTART: /Users/clairerhoda/workspace/comp_sci_10/lab4_prog6.py ======
+##Enter two space separated words:listen silent
+##The words listen and silent are anangrams.
+##
+##Press Enter to quit
+##
+##Test Case 5
+##>>> 
+##====== RESTART: /Users/clairerhoda/workspace/comp_sci_10/lab4_prog6.py ======
+##Enter two space separated words:done bun
+##The words done and bun are not anangrams.
+##
+##Press Enter to quit
+##
+##Test Case 6
+##>>> 
+##====== RESTART: /Users/clairerhoda/workspace/comp_sci_10/lab4_prog6.py ======
+##Enter two space separated words:anagram margana
+##The words anagram and margana are anangrams.
+##
+##Press Enter to quit
+##>>> 
