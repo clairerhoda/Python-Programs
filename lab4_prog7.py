7c59b80a0d588f1b0ec84a4de4f29224fb0374d2
Claire Rhoda <clairehrhoda@gmail.com>
2019-04-19 13:58:17 -0700

    added comments

--- lab4_prog7.py
+++ lab4_prog7.py
@@ -4,17 +4,20 @@
 #This program creates new sets of elements from the three given sets
 
 def aFunc(set1:set,set2:set)->set:
-    setA = set1^set2
+    """elements that are in either set1 or set2"""
+    setA = set1^set2  
     return setA
 
 
 def bFunc(set1:set,set2:set,set3:set)->set:
+    """elements that are in only one of the three sets"""
     setB1 = set1^set2
     setB2 = setB1^set3
     return setB2
 
 
 def cFunc(set1:set,set2:set,set3:set)->set:
+    """elements that are in exactly two of the three sets"""
     setC1 = set1&set2
     setC2 = set1&set3
     setC3 = set2&set3  #NO MATCHES BUT CHECKS ANYWAY
@@ -23,6 +26,7 @@ def cFunc(set1:set,set2:set,set3:set)->set:
 
 
 def dFunc(set1:set)->set:
+    """elements that are in range 1-25 but not in set1"""
     theRange = {1,2,3,4,5,6,7,8,9,10,11,12,13, \
         14,15,16,17,18,19,20,21,22,23,24,25}
     setD = theRange - set1
@@ -30,6 +34,7 @@ def dFunc(set1:set)->set:
 
 
 def eFunc(set1:set,set2:set,set3:set)->set:
+    """elements that are in range 1-25 but not in any of the three elements"""
     theRange = {1,2,3,4,5,6,7,8,9,10,11,12,13, \
         14,15,16,17,18,19,20,21,22,23,24,25}
     setE1 = theRange - set1
@@ -39,6 +44,7 @@ def eFunc(set1:set,set2:set,set3:set)->set:
 
 
 def fFunc(set1:set,set2:set,set3:set)->set:
+    """elements in range 1 to 25 that are not in all three sets"""
     theRange = {1,2,3,4,5,6,7,8,9,10,11,12,13, \
         14,15,16,17,18,19,20,21,22,23,24,25}
     setF1 = set1&set2&set3
@@ -47,18 +53,21 @@ def fFunc(set1:set,set2:set,set3:set)->set:
 
 
 def main():
+    #the main sets used by different functions above
     set1 = {1,2,3,4,5}
     set2 = {2,4,6,8}
     set3 = {1,5,9,13,17} 
 
+    #calls each function for each separate task
     a = aFunc(set1,set2)
     b = bFunc(set1,set2,set3)
     c = cFunc(set1,set2,set3)
     d = dFunc(set1)
     e = eFunc(set1,set2,set3)
     f = fFunc(set1,set2,set3)
-    
-    print("a.",a,sep='')
+
+   #prints all the returns 
+    print("a.",a,sep='')  
     print("b.",b,sep='')
     print("c.",c,sep='')
     print("d.",d,sep='')
